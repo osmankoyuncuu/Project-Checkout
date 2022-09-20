f629aaf95639542665f183bd4139dbaa8527a9ec
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-09-20 22:57:01 +0300

    js finish

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 //? Selectors
 
 const cardBox = document.querySelector(".card-box");
+const subTotalDom = document.querySelector("#subTotal");
+const taxDom = document.querySelector("#tax");
+const shippingDom = document.querySelector("#shipping");
+totalDom = document.querySelector("#total");
 
+//?onload
+let shipping = 19;
+let subTotal = 0;
+const pTotal = document.querySelectorAll(".pTotal");
+pTotal.forEach((e) => {
+  const productTotal =
+    Number(
+      e.parentElement.parentElement.parentElement.children[1].children[0]
+        .innerText
+    ) *
+    e.parentElement.parentElement.parentElement.children[2].children[1]
+      .innerText;
+  subTotal += productTotal;
+  e.innerText = productTotal.toFixed(2);
+});
+subTotalDom.innerText = subTotal.toFixed(2);
+taxDom.innerText = (subTotal * 0.18).toFixed(2);
+shippingDom.innerText = shipping;
+totalDom.innerText = (subTotal + subTotal * 0.18 + shipping).toFixed(2);
+
+//? onclick
 cardBox.addEventListener("click", (e) => {
   //? minus button
   let pieceText = e.target.parentElement.children[1];
@@ -35,5 +60,28 @@ cardBox.addEventListener("click", (e) => {
   }
 
   //?Product totals
-  //? todo:
+  let productTotal;
+  subTotal = 0;
+  pTotal.forEach((e) => {
+    productTotal =
+      Number(
+        e.parentElement.parentElement.parentElement.children[1].children[0]
+          .innerText
+      ) *
+      e.parentElement.parentElement.parentElement.children[2].children[1]
+        .innerText;
+    e.innerText = productTotal.toFixed(2);
+    subTotal += Number(e.innerText);
+  });
+  //? Result
+  subTotalDom.innerText = subTotal.toFixed(2);
+  taxDom.innerText = (subTotal * 0.18).toFixed(2);
+  if (subTotal > 400) {
+    shipping = 0;
+    shippingDom.innerText = shipping;
+  } else {
+    shipping = 19;
+    shippingDom.innerText = shipping;
+  }
+  totalDom.innerText = (subTotal + subTotal * 0.18 + shipping).toFixed(2);
 });
