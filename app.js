e174e081a2f01e330113f67aa3b6c0be3fc85c70
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-09-21 22:34:18 +0300

    commit

--- app.js
+++ app.js
@@ -4,68 +4,65 @@ const cardBox = document.querySelector(".card-box");
 const subTotalDom = document.querySelector("#subTotal");
 const taxDom = document.querySelector("#tax");
 const shippingDom = document.querySelector("#shipping");
-totalDom = document.querySelector("#total");
+const totalDom = document.querySelector("#total");
 
 //?onload
-let shipping = 19;
-let subTotal = 0;
+const shipping = 19;
+const shippingFree = 0;
+const tax = 0.18;
 const pTotal = document.querySelectorAll(".pTotal");
-pTotal.forEach((e) => {
-  const productTotal =
-    Number(
-      e.parentElement.parentElement.parentElement.children[1].children[0]
-        .innerText
-    ) *
-    e.parentElement.parentElement.parentElement.children[2].children[1]
-      .innerText;
-  subTotal += productTotal;
-  e.innerText = productTotal.toFixed(2);
+
+window.addEventListener("load", () => {
+  calculate();
 });
-subTotalDom.innerText = subTotal.toFixed(2);
-taxDom.innerText = (subTotal * 0.18).toFixed(2);
-shippingDom.innerText = shipping;
-totalDom.innerText = (subTotal + subTotal * 0.18 + shipping).toFixed(2);
 
 //? onclick
 cardBox.addEventListener("click", (e) => {
   //? minus button
-  let pieceText = e.target.parentElement.children[1];
   if (e.target.classList.contains("minus")) {
-    if (pieceText.innerText > 1) {
-      pieceText.innerText--;
+    if (e.target.nextElementSibling.innerText > 1) {
+      e.target.nextElementSibling.innerText--;
+    } else {
+      if (confirm("Product will be removed?")) {
+        e.target.parentElement.parentElement.parentElement.remove();
+        calculate();
+      }
     }
+    calculate();
   }
   //? plus button
   if (e.target.classList.contains("plus")) {
-    if (pieceText.innerText >= 1) {
-      pieceText.innerText++;
+    if (e.target.previousElementSibling.innerText >= 1) {
+      e.target.previousElementSibling.innerText++;
     }
+    calculate();
   }
 
   //? remove button
   const result = document.querySelector(".result");
-  if (
-    e.target.parentElement.parentElement.parentElement.parentElement.children
-      .length -
-      1 ===
-    0
-  ) {
-    e.target.parentElement.parentElement.parentElement.parentElement.remove();
-    result.style.marginTop = "2rem";
-    result.style.fontSize = "2rem";
-    result.parentElement.style.height = "40rem";
-    result.innerText = `Your cart is empty !!`;
-  } else if (e.target.classList.contains("remove-button")) {
-    e.target.parentElement.parentElement.parentElement.remove();
+  if (e.target.classList.contains("remove-button")) {
+    if (
+      e.target.parentElement.parentElement.parentElement.parentElement
+        .childElementCount === 1
+    ) {
+      e.target.parentElement.parentElement.parentElement.parentElement.remove();
+      result.style.marginTop = "2rem";
+      result.style.fontSize = "2rem";
+      result.parentElement.style.height = "40rem";
+      result.innerText = `Your cart is empty !!`;
+    } else if (e.target.classList.contains("remove-button")) {
+      e.target.parentElement.parentElement.parentElement.remove();
+      calculate();
+    }
   }
+});
 
-  //?Product totals
-  let productTotal;
+const calculate = () => {
   subTotal = 0;
   pTotal.forEach((e) => {
     productTotal =
       Number(
-        e.parentElement.parentElement.parentElement.children[1].children[0]
+        e.parentElement.parentElement.parentElement.children[1].children[1]
           .innerText
       ) *
       e.parentElement.parentElement.parentElement.children[2].children[1]
@@ -75,13 +72,11 @@ cardBox.addEventListener("click", (e) => {
   });
   //? Result
   subTotalDom.innerText = subTotal.toFixed(2);
-  taxDom.innerText = (subTotal * 0.18).toFixed(2);
-  if (subTotal > 400) {
-    shipping = 0;
-    shippingDom.innerText = shipping;
+  taxDom.innerText = (subTotal * tax).toFixed(2);
+  if (subTotal > 300) {
+    shippingDom.innerText = shippingFree;
   } else {
-    shipping = 19;
     shippingDom.innerText = shipping;
   }
-  totalDom.innerText = (subTotal + subTotal * 0.18 + shipping).toFixed(2);
-});
+  totalDom.innerText = (subTotal + subTotal * tax + shipping).toFixed(2);
+};
